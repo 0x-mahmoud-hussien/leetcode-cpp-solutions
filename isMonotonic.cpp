0e8964e3d5b5638991b57788696e2af0c30afb63
Mahmoud Hussien <mahmoud.abdelmaqsoud.hussien@gmail.com>
2025-10-03 15:13:43 +0300

    Add solution for Monotonic Array

---
+++ isMonotonic.cpp
@@ -0,0 +1,36 @@
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution 
+{
+public:
+    bool isMonotonic(vector<int>& nums) 
+    {
+        bool inc = true, dec = true;
+
+        for (int i = 1; i < nums.size(); i++) 
+        {
+            if (nums[i] > nums[i - 1]) dec = false; 
+            if (nums[i] < nums[i - 1]) inc = false; 
+        }
+
+        return inc || dec;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> nums1 = {1, 2, 2, 3};
+    vector<int> nums2 = {6, 5, 4, 4};
+    vector<int> nums3 = {1, 3, 2};
+    cout << boolalpha << endl;
+    cout << sol.isMonotonic(nums1) << endl; 
+    cout << sol.isMonotonic(nums2) << endl; 
+    cout << sol.isMonotonic(nums3) << endl; 
+
+    return 0;
+}
