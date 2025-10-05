48693b72709120918104791f16e775c8ea3cbbf0
Mahmoud Hussien <mahmoud.abdelmaqsoud.hussien@gmail.com>
2025-10-05 20:54:15 +0300

    Add solution for Reverse String II

---
+++ reverseStr.cpp
@@ -0,0 +1,29 @@
+
+// https://leetcode.com/problems/reverse-string-ii
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string reverseStr(string s, int k) 
+    {
+        for (int i = 0; i < s.size(); i += 2 * k) 
+        {
+            if (i + k <= s.size())
+                reverse(s.begin() + i, s.begin() + i + k);
+            else
+                reverse(s.begin() + i, s.end());
+        }
+        return s;
+    }
+};
+
+int main() 
+{
+    Solution sol;
+    string s = "abcdefg";
+    int k = 2;
+    cout << sol.reverseStr(s, k); 
+}
